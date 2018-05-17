58d5fa3b554bade9587bd753671b7ec80da2fbf8
nakazono <nakazono@i-hearts.jp>
2018-05-18 08:45:11 +0900

    fix

--- page97.kt
+++ page97.kt
@@ -6,24 +6,29 @@
 package practiceP97
 
 fun main(args: Array<String>) {
-	
+    
     // ラムダ式と無名関数の違い
     // 　無名関数は値を帰す場合、return必須
     // 　無名関数は非ローカルリターン使用不可
     
     // Kotlinではラムダ式、無名関数をまとめて「関数リテラル」という
     
-	// ラムダ式
-	val square1: (Int) -> Int = { i: Int ->
-		i * i
-	}
+    // ラムダ式
+    val square1: (Int) -> Int = { i: Int ->
+        i * i
+    }
     
     // 無名関数
-    val square2: (int) -> Int = fun(i: Int) Int {
+    val square2: (Int) -> Int = fun(i: Int): Int {
         return i * i
     }
+
     // 無名関数(省略ver)
-    val square3: (int) -> Int = fun(i: Int) = i * i
+    val square3: (Int) -> Int = fun(i: Int) = i * i
+    
+    println(square1(2))
+    println(square2(2))
+    println(square3(2))
 }
 
 
